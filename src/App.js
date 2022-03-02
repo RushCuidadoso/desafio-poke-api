87f6623c528853009669ca51929b9eaee4770291
RushCuidadoso <d3nisfp@gmail.com>
2022-03-02 08:47:50 -0400

    casa 02/03

--- src/App.js
+++ src/App.js
@@ -2,40 +2,52 @@ import { useEffect, useState } from "react";
 import PokemonThumbnail from "./components/PokemonThumbnail";
 
 function App() {
+  const [allPokemons, setAllPokemon] = useState([]);
+  const [loadMore, setLoadMore] = useState("https://pokeapi.co/api/v2/pokemon/?limit=20");
 
-  const [allPokemons, setAllPokemon] = useState([])
-  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon/?limit=20')
+  const getAllPokemons = async function () {
+    const response = await fetch(loadMore);
+    const data = await response.json();
 
-  const getAllPokemons = async function(){
-    const response = await fetch(loadMore)
-    const data = await response.json()
+    setLoadMore(data.next);
 
-    setLoadMore(data.next)
+    createPokemon(data.results);
 
-    function createPokemon(results){
+    function createPokemon(results) {
       results.forEach(async (pokemon) => {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
-        const data = await response.json()
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+        var data = await response.json();
 
-        setAllPokemon(currentList => [...currentList, data])
+        
+
+        console.log(data)
+
+        setAllPokemon((currentList) => [...currentList, data]);
       });
     }
-    createPokemon(data.results)
-    await console.log(allPokemons)
-  }
+  };
 
-  useEffect(()=>{
-    getAllPokemons()
-  }, [])
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
 
   return (
     <div className="app-container">
       <h1>Desafio Pokemon</h1>
       <div className="pokemon-container">
         <div className="all-container">
-          {allPokemons.map((pokemon, index) =><PokemonThumbnail id={pokemon.id} name={pokemon.name} image={pokemon.sprites.other.dream_world.front_default} type={pokemon.types[0].type.name}/> )}
+          {allPokemons.map((pokemon, index) => (
+            <PokemonThumbnail
+              id={pokemon.id}
+              name={pokemon.name}
+              image={pokemon.sprites.other.dream_world.front_default}
+              type={pokemon.types[0].type.name}
+            />
+          ))}
         </div>
-        <button className="load-more" onClick={() => getAllPokemons()}>Carregar mais</button>
+        <button className="load-more" onClick={() => getAllPokemons()}>
+          Carregar mais
+        </button>
       </div>
     </div>
   );
