25174cc4f7971986c8d8ef5d354e1da19f15d2e2
RushCuidadoso <d3nisfp@gmail.com>
2022-03-04 12:49:10 -0400

    Paginamento funcionando

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import PokemonThumbnail from "./components/PokemonThumbnail";
 import Pagination from "./Pagination";
 
+var loadedPages = 0
+var currentPage = 1
+
 function App() {
   // Declaração de use states para o código
   const [allPokemons, setAllPokemon] = useState([]);
@@ -11,19 +14,44 @@ function App() {
   const [nextPageUrl, setNextPageUrl] = useState();
   const [prevPageUrl, setPrevPageUrl] = useState();
   const [loading, setLoading] = useState(true); // Estado utilizado para indicar se a página está carregando
-  const [loadedPages, setLoadedPages] = useState(1);
-  const [currentPage, setCurrentPage] = useState(1);
-
+  // const [loadedPages, setLoadedPages] = useState(0);
+  //const [currentPage, setCurrentPage] = useState(1);
 
   // Função que pega todos os pokemons de 20 em 20
-  const getAllPokemons = async function () {
-    if (currentPage >= loadedPages) {
-      const response = await fetch(CurrentPageUrl);
+  const getAllPokemons = function () {
+    if (currentPage > loadedPages || loadedPages === 0 ) {
+      loadPokemons();
+
+      // Código utilizando localStorage, para evitar requisitar informações que já foram usadas
+      // TODO fazer funcionar.
+    } else {
+
+      if (currentPage === 1) {
+        for (let index = 1; index <= 20; index++) {
+          let info = JSON.parse(localStorage.getItem(index));
+          setAllPokemon((currentList) => [...currentList, info]);
+        }
+      } else {
+        let showItems = 1 + (20 * (currentPage - 1)) ;
+        for (let index = showItems; index <= showItems + 19; index++) {
+          let info = JSON.parse(localStorage.getItem(index));
+          setAllPokemon((currentList) => [...currentList, info]);
+        }
+      }
+    }
+    setLoading(false);
+    console.log("loadedPages", loadedPages);
+    console.log("currentPage", currentPage);
+  };
+
+  const loadPokemons = async function(){
+    const response = await fetch(CurrentPageUrl);
       const data = await response.json();
       setNextPageUrl(data.next);
       setPrevPageUrl(data.previous);
       createPokemon(data.results);
-      setLoadedPages(loadedPages + 1);
+      loadedPages = loadedPages + 1;
+      console.log('why loaded pages', loadedPages)
 
       // Função que pega os detalhes dos 20 pokemons
       function createPokemon(results) {
@@ -32,15 +60,24 @@ function App() {
             `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
           );
           var data = await response.json();
-
           // Adiciona as informações em localStorage para utilizar depois, caso o usuário volte as páginas
           localStorage.setItem(
             data.id,
             JSON.stringify({
               id: data.id,
               name: data.name,
-              image: data.sprites.other.dream_world.front_default,
-              type: data.types[0].type.name,
+              sprites: {
+                other:{
+                  dream_world:{
+                    front_default: data.sprites.other.dream_world.front_default
+                  }
+                }
+              },
+              types: [{
+                type:{
+                  name: data.types[0].type.name
+                }
+              }],
             })
           );
           // Adiciona os pokemons em uma lista para mostrar
@@ -48,47 +85,25 @@ function App() {
         });
         setLoading(false);
       }
-
-      // Código utilizando localStorage, para evitar requisitar informações que já foram usadas
-      // TODO fazer funcionar.
-    } else {
-      if (currentPage === 1) {
-        for (let index = 1; index <= 20; index++) {
-          let info = JSON.parse(localStorage.getItem(index));
-          setAllPokemon((currentList) => [...currentList, info]);
-          console.log(info);
-        }
-      } else {
-        let showItems = currentPage + 20 * currentPage;
-        for (let index = showItems; index <= showItems + 19; index++) {
-          let info = JSON.parse(localStorage.getItem(index));
-
-          setAllPokemon((currentList) => [...currentList, info]);
-          console.log(info);
-        }
-      }
-    }
-  };
+  }
 
   // useEffect por cada vez que a página muda, é chamada outra lista
   useEffect(() => {
     setLoading(true);
     getAllPokemons();
-    console.log("loadedPages", loadedPages);
-    console.log("currentPage", currentPage);
-  }, [CurrentPageUrl]);
+  }, [currentPage]);
 
 
   // Funções de mudança de página
   function gotoNextPage() {
     setCurrentPageUrl(nextPageUrl);
-    setCurrentPage(currentPage + 1);
+    currentPage = currentPage + 1;
     setAllPokemon([]);
   }
 
   function gotoPrevPage() {
     setCurrentPageUrl(prevPageUrl);
-    setCurrentPage(currentPage - 1);
+    currentPage = currentPage - 1;
     setAllPokemon([]);
   }
 
@@ -113,7 +128,7 @@ function App() {
         </div>
         <Pagination
           gotoNextPage={nextPageUrl ? gotoNextPage : null}
-          gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
+          gotoPrevPage={(currentPage !== 1) ? gotoPrevPage : null}
         />
       </div>
     </div>
